19341c2e81c87ea52d37278e194fb860d55a6a70
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-06-23 08:54:04 -0300

    Update loading animation

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, SafeAreaView, Text, View, FlatList } from "react-native"
 import colors from "../../styles/colors"
 import fonts from "../../styles/fonts"
 
+import { Load } from "../components/Load"
 import { EnvironmentButton } from "../components/EnvironmentButton"
 import { Header } from "../components/Header"
 import { PlantCardPrimary } from "../components/PlantCardPrimary"
@@ -30,8 +31,32 @@ interface PlantProps {
 export const PlantSelect: React.FC = () => {
 	const [environments, setEnvironments] = useState<EnvironmentProps[]>([])
 	const [plants, setPlants] = useState<PlantProps[]>([])
-	const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([])
+	const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>(plants)
 	const [environmentSelected, setEnvironmentSelected] = useState("all")
+
+	const [loading, setLoading] = useState(true)
+
+	const [page, setPage] = useState(1)
+	const [loadingMore, setLoadingMore] = useState(false)
+	const [loadedAll, setLoadedAll] = useState(false)
+
+	function fetchPlants(): void {
+		api.get(`/plants?_sort=name&_order=asc&_page=${page}&_limit=8`).then(
+			({ data }) => {
+				if (!data) return setLoading(true)
+				if (page > 1) {
+					setPlants(oldValue => [...oldValue, ...data])
+					setFilteredPlants(oldValue => [...oldValue, ...data])
+				} else {
+					setPlants(data)
+					setFilteredPlants(data)
+				}
+
+				setLoading(false)
+				setLoadingMore(false)
+			}
+		)
+	}
 
 	function handleEnvironmentSelected(environment: string): void {
 		setEnvironmentSelected(environment)
@@ -44,6 +69,13 @@ export const PlantSelect: React.FC = () => {
 
 		setFilteredPlants(filtered)
 	}
+	function handleFetchMore(distance: number): void {
+		if (distance < 1) return
+
+		setLoadingMore(true)
+		setPage(oldValue => oldValue + 1)
+		fetchPlants()
+	}
 
 	useEffect(() => {
 		api.get("/plants_environments?_sort=title&_order=asc").then(response => {
@@ -55,10 +87,7 @@ export const PlantSelect: React.FC = () => {
 				...response.data
 			])
 		})
-
-		api.get("/plants?_sort=name&_order=asc").then(response => {
-			setPlants(response.data)
-		})
+		fetchPlants()
 
 		// async function fetchEnvironment(): void {
 		// 	const { data } = await api.get("plants_environments")
@@ -67,7 +96,9 @@ export const PlantSelect: React.FC = () => {
 		// fetchEnvironment()
 	}, [])
 
-	return (
+	return loading ? (
+		<Load />
+	) : (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.header}>
 				<Header />
@@ -97,6 +128,10 @@ export const PlantSelect: React.FC = () => {
 					renderItem={({ item }) => <PlantCardPrimary data={item} />}
 					showsVerticalScrollIndicator={false}
 					numColumns={2}
+					onEndReachedThreshold={0.1}
+					onEndReached={({ distanceFromEnd }) =>
+						handleFetchMore(distanceFromEnd)
+					}
 				/>
 			</View>
 		</SafeAreaView>
