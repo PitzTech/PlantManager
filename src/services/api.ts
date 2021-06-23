8b32a13fc337c99eb4a3cb5457bcc24759fb0f66
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-06-23 11:05:52 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-	baseURL: "https://26712aedc569.ngrok.io/"
+	//baseURL: "https://26712aedc569.ngrok.io/"
+	baseURL: "http://localhost:3333"
 })
 
 export default api
